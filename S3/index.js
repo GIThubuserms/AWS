8631b4172ee5368bfc4b6420cc8b7bec68fc7bc4
Murtaza0318 <murtazasaleem2004@gmail.com>
2025-05-07 12:34:45 +0500

    done with s3 getobj

---
+++ S3/index.js
@@ -0,0 +1,30 @@
+
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const client = new S3Client(
+    { 
+        region: "us-east-1",
+        credentials:{
+            accessKeyId:"AKIAZ3OSZ3RDL3NMD36S",
+            secretAccessKey:"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"
+        }
+    }
+);
+
+// we created a client of that nodejs user cred now link include that data 
+// we craeted command including bucketname and filename
+// we call func and pass the args
+
+async function geturl(key){
+    const command=new GetObjectCommand({
+        Bucket:"murtazaprivbucket",
+        Key:key
+    })
+   
+    const url=await getSignedUrl(client,command)
+    return url
+}
+
+console.log(await geturl("download.png"))
+
