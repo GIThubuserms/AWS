8844e5ff3576c5b5a5c59e93e34c40561068d47d
Murtaza0318 <murtazasaleem2004@gmail.com>
2025-05-07 12:40:36 +0500

    done with s3 getobj

--- S3/index.js
+++ S3/index.js
@@ -1,30 +1,30 @@
 
-import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+// import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+// import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-const client = new S3Client(
-    { 
-        region: "us-east-1",
-        credentials:{
-            accessKeyId:
-            secretAccessKey:
-        }
-    }
-);
+// const client = new S3Client(
+//     { 
+//         region: "us-east-1",
+//         credentials:{
+//             accessKeyId:
+//             secretAccessKey:
+//         }
+//     }
+// );
 
-// we created a client of that nodejs user cred now link include that data 
-// we craeted command including bucketname and filename
-// we call func and pass the args
+// // we created a client of that nodejs user cred now link include that data 
+// // we craeted command including bucketname and filename
+// // we call func and pass the args
 
-async function geturl(key){
-    const command=new GetObjectCommand({
-        Bucket:"murtazaprivbucket",
-        Key:key
-    })
+// async function geturl(key){
+//     const command=new GetObjectCommand({
+//         Bucket:"murtazaprivbucket",
+//         Key:key
+//     })
    
-    const url=await getSignedUrl(client,command)
-    return url
-}
+//     const url=await getSignedUrl(client,command)
+//     return url
+// }
 
-console.log(await geturl("download.png"))
+// console.log(await geturl("download.png"))
 
