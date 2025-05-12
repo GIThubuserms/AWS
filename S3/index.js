da1b5b5e011ab3a8b07384e532ee6f6820735531
Murtaza0318 <murtazasaleem2004@gmail.com>
2025-05-12 23:40:50 +0500

    done with services

--- S3/index.js
+++ S3/index.js
@@ -1,30 +1,42 @@
 
-// import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
-// import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { S3Client, GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-// const client = new S3Client(
-//     { 
-//         region: "us-east-1",
-//         credentials:{
-//             accessKeyId:
-//             secretAccessKey:
-//         }
-//     }
-// );
+const client = new S3Client(
+    { 
+        region: "us-east-1",
+        credentials:{
+            accessKeyId:"AKIAZ3OSZ3RDL3NMD36S",
+            secretAccessKey:"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"
+        }
+    }
+);
 
-// // we created a client of that nodejs user cred now link include that data 
-// // we craeted command including bucketname and filename
-// // we call func and pass the args
+// we created a client of that nodejs user cred now link include that data 
+// we craeted command including bucketname and filename
+// we call func and pass the args
 
-// async function geturl(key){
-//     const command=new GetObjectCommand({
-//         Bucket:"murtazaprivbucket",
-//         Key:key
-//     })
+async function geturl1(key){
+    const command=new GetObjectCommand({
+        Bucket:"murtazaprivbucket",
+        Key:key
+    })   
+    const url=await getSignedUrl(client,command)
+    return url
+}
+
+
+async function geturl2(filename,type){
+    const command =new PutObjectCommand({
+        Bucket:"murtazaprivbucket",
+        Key:`uploads/${filename}`,
+        ContentType:type
+    })
    
-//     const url=await getSignedUrl(client,command)
-//     return url
-// }
+    const url=await getSignedUrl(client,command)
+    return url
+}
 
-// console.log(await geturl("download.png"))
+console.log(await geturl1("uploads/1746613505456.jpeg"))
+// console.log(await geturl2(`${Date.now()}.jpeg`,"jpeg"))
 
